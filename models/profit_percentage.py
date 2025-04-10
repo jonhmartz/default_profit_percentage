879dae914c966d8544c22b0664d325e76d9ee1bb
jonhmartz <37033096+jonhmartz@users.noreply.github.com>
2025-04-09 22:51:50 -0600

    Add files via upload

--- models/profit_percentage.py
+++ models/profit_percentage.py
@@ -1,14 +1,23 @@
 from odoo import api, fields, models
 
-class ProfitPercentageConfig(models.Model):
+class ProfitPercentageConfig(models.TransientModel):
     _name = 'profit.percentage.config'
     _description = 'Profit Percentage Configuration'
     
-    value = fields.Float(string='Default Profit Percentage', default=20.0)
+    value = fields.Float(
+        string='Default Profit Percentage', 
+        default=lambda self: self._get_default_value(),
+        help='Default percentage to calculate profit margins'
+    )
+    
+    def _get_default_value(self):
+        return float(self.env['ir.config_parameter'].sudo().get_param('profit_percentage.default_value', '20.0'))
+    
+    def set_default(self):
+        self.ensure_one()
+        self.env['ir.config_parameter'].sudo().set_param('profit_percentage.default_value', str(self.value))
+        return {'type': 'ir.actions.client', 'tag': 'reload'}
     
     @api.model
-    def get_default(self):
-        config = self.search([], limit=1)
-        if not config:
-            config = self.create({'value': 20.0})
-        return config.value
+    def get_default_percentage(self):
+        return float(self.env['ir.config_parameter'].sudo().get_param('profit_percentage.default_value', '20.0'))
