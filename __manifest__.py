ebc8a5c53d9150424abfaa6b22861af88adf4ceb
jonhmartz <37033096+jonhmartz@users.noreply.github.com>
2025-04-09 23:25:41 -0600

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -3,15 +3,16 @@
     'version': '1.0',
     'summary': 'Set default profit percentage',
     'description': """
-        Configure a system-wide default profit percentage.
+        Configure a system-wide default profit percentage and add it to sales order lines.
     """,
     'category': 'Sales',
     'author': 'Your Company',
     'website': 'https://www.yourcompany.com',
-    'depends': ['base'],
+    'depends': ['base', 'sale'],  # Añadimos la dependencia de 'sale'
     'data': [
         'security/ir.model.access.csv',
         'views/profit_percentage_view.xml',
+        'views/sale_views.xml',  # Nuevo archivo para las vistas de ventas
     ],
     'installable': True,
     'application': True,
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from . import profit_percentage
+from . import sale_order_line
